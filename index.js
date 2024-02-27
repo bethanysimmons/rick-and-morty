c5dcae9fa5cba3ca876240ddb03918f9b2a34870
bethanysimmons <157748151+bethanysimmons@users.noreply.github.com>
2024-02-26 21:29:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+// Import JSON dataset
+const data = require("./data.json")
+const app = express()
+const port = 3000   
+
+// Tell nunjucks where your template files are located (e.g., 'views' directory)
+nunjucks.configure('views', {
+    autoescape: true,
+    noCache: true, // <-- Should only be true when developing
+    express: app
+});
+
+
+// Endpoint for /characters shows all characters
+app.get('/characters', function (req, res) {
+    res.render('default.njk', { 
+        title: "Rick and Morty Characters", 
+        data: data
+    });
+});
+
+// Endpoint for /characters/:id shows details for ONE characer
+app.get('/character/:id', function (req, res) {
+
+    let id = req.params.id
+
+    res.render('character.njk', { 
+        id: id,
+        character: data.results[id-1]
+});
+});
+
+// app.get('/character/:id', function (req, res) {
+//     const id = req.params.id;
+
+//     const character= data.results.find(char=> char.id == id);
+//     res.render('character.njk', {character: character});
+//     });
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
